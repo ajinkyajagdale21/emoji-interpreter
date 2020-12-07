cc57d9fd56685cfe564757adf097f11199eeb030
ajinkyajagdale21 <68507834+ajinkyajagdale21@users.noreply.github.com>
2020-12-07 21:26:08 +0530

    upddate:1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  var counter = 0;
+  function likeEventhandler() {
+    console.log("clicked" + counter);
+    counter++;
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Inside Outtt</h1>
+
+      <button onClick={likeEventhandler}>like</button>
     </div>
   );
 }
